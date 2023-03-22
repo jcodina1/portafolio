75a3ded6b8757461e1770985b420decc2fd21e1f
Juan Codina <codina844@gmail.com>
2023-03-21 21:49:05 -0500

    correcion tamaño de imagen de error de 404

--- src/components/404/Error404.jsx
+++ src/components/404/Error404.jsx
@@ -1,6 +1,6 @@
 import error404 from "../../assets/Frame1.svg"
 export default function Error404(){
     return(
-        <img src={error404} alt="" />
+        <img src={error404} alt="" style={{width:'100%'}}/>
     )
 }
